9d772005fec84a1b1f07e55c407d634e620aaf48
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-02 12:14:56 +0300

    show list using dummy data

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './MoviesList.css';
+import React from "react";
+import { Row, Col, Card } from "antd";
+import { variables } from "../../config/variables";
+import PropTypes from "prop-types";
+import "./MoviesList.css";
+import movies from "../../data.json";
 
-const MoviesList = () => (
-  <div className="MoviesList">
-    MoviesList Component
-  </div>
-);
+const { Meta } = Card;
+
+const MoviesList = () => {
+  return (
+    <div className="MoviesList">
+      <Row>
+        {movies.data.results.map((movie) => (
+          <Col key={movie.id} xs={24} sm={12} md={10} lg={8} xl={6}>
+            <Card
+              className="movie-card"
+              hoverable
+              cover={
+                <img
+                  alt={movie["title"]}
+                  src={`${variables.imagesBaseUrl}${movie["poster_path"]}`}
+                />
+              }
+            >
+              <Meta title={movie["title"]} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
 MoviesList.propTypes = {};
 
