c1f0ee5552855979777a6b3f1b3d8610c0c142f2
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-02 12:30:18 +0300

    load movies from api

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Card } from "antd";
+import apiClient from "../../apiClient";
 import { variables } from "../../config/variables";
 import PropTypes from "prop-types";
 import "./MoviesList.css";
-import movies from "../../data.json";
 
 const { Meta } = Card;
 
 const MoviesList = () => {
+  const [moviesData, setMoviesData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    apiClient
+      .get("/popular", {
+        params: {
+          page: 1,
+        },
+      })
+      .then((response) => {
+        setMoviesData(response?.data);
+      })
+      .catch((error) => {
+        console.log(
+          "🚀 ~ file: MoviesList.js ~ line 21 ~ useEffect ~ error",
+          error
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {};
+  }, []);
+
   return (
     <div className="MoviesList">
       <Row>
-        {movies.data.results.map((movie) => (
+        {moviesData?.data?.results.map((movie) => (
           <Col key={movie.id} xs={24} sm={12} md={10} lg={8} xl={6}>
             <Card
               className="movie-card"
