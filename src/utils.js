9f41ad97b98a32da04a6fefe23d09c286df5d199
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-02 21:15:55 +0300

    add go to top util

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,9 @@
 export const createArrayFromRange = (min, max) =>
   Array.from({ length: max - min + 1 }, (_, i) => min + i);
+
+export const goToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
