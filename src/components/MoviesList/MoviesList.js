cc8f0174f35358ca9f9ae85cac451aa0d8bc8e13
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-02 13:01:38 +0300

    handle loading states

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Spin, Alert } from "antd";
 import apiClient from "../../apiClient";
 import { variables } from "../../config/variables";
 import PropTypes from "prop-types";
 import "./MoviesList.css";
+import CenteredSpace from "../CenteredSpace/CenteredSpace";
 
 const { Meta } = Card;
 
 const MoviesList = () => {
   const [moviesData, setMoviesData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [loadingState, setLoadingState] = useState({
+    type: "default",
+    message: "",
+  });
 
   useEffect(() => {
-    setIsLoading(true);
+    setLoadingState({ type: "fetching" });
     apiClient
       .get("/popular", {
         params: {
@@ -21,15 +25,10 @@ const MoviesList = () => {
       })
       .then((response) => {
         setMoviesData(response?.data);
+        setLoadingState({ type: "success" });
       })
       .catch((error) => {
-        console.log(
-          "🚀 ~ file: MoviesList.js ~ line 21 ~ useEffect ~ error",
-          error
-        );
-      })
-      .finally(() => {
-        setIsLoading(false);
+        setLoadingState({ type: "error", message: error.message });
       });
 
     return () => {};
@@ -38,22 +37,36 @@ const MoviesList = () => {
   return (
     <div className="MoviesList">
       <Row>
-        {moviesData?.data?.results.map((movie) => (
-          <Col key={movie.id} xs={24} sm={12} md={10} lg={8} xl={6}>
-            <Card
-              className="movie-card"
-              hoverable
-              cover={
-                <img
-                  alt={movie["title"]}
-                  src={`${variables.imagesBaseUrl}${movie["poster_path"]}`}
-                />
-              }
-            >
-              <Meta title={movie["title"]} />
-            </Card>
-          </Col>
-        ))}
+        {loadingState?.type === "fetching" ? (
+          <CenteredSpace>
+            <Spin size="large" />
+          </CenteredSpace>
+        ) : loadingState?.type === "error" ? (
+          <CenteredSpace>
+            <Alert
+              message="An error occurred"
+              description={loadingState.message}
+              type="error"
+            />
+          </CenteredSpace>
+        ) : (
+          moviesData?.data?.results.map((movie) => (
+            <Col key={movie.id} xs={24} sm={12} md={10} lg={8} xl={6}>
+              <Card
+                className="movie-card"
+                hoverable
+                cover={
+                  <img
+                    alt={movie["title"]}
+                    src={`${variables.imagesBaseUrl}${movie["poster_path"]}`}
+                  />
+                }
+              >
+                <Meta title={movie["title"]} />
+              </Card>
+            </Col>
+          ))
+        )}
       </Row>
     </div>
   );
